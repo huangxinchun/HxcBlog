11608bb75a667cb2413c3de8d3e48b22bfdd962a
hxc <173315279@qq.com>
2020-04-12 23:47:19 +0800

    test

---
+++ Language/Php/push.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+message=$1
+book="php.md"
+book_bak="2020-04-12-my-$book.md"
+#更新自己的分支
+echo "更新开始！"
+cp $book ../../../hxc.github.io/_posts/$book
+git add ./
+git commit -m "$message"
+git push 
+
+echo "更新完成！"
+echo "更新博客开始！"
+
+cd ../hxc.github.io/_posts/
+cp $book $book_bak
+
+#添加固定的头
+echo "---
+layout:     post                    # 使用的布局（不需要改）
+title:      PHP总结              # 标题 
+subtitle:   坚持坚持再坚持      #副标题
+date:       2020-04-13              # 时间
+author:     hxc                      # 作者
+header-img:    #这篇文章标题背景图片
+catalog: true                       # 是否归档
+tags:                               #标签
+    - PHP
+---" > $book 
+#写入内容
+cat $book_bak >> $book
+#干掉备份文件
+rm -f $book_bak
+git add ./
+git commit -m "$message"
+git push origin master
+echo "更新博客结束！"
