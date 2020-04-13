ef9d7c835a5c82c259e1050bcde5850d84dc1e99
hxc <173315279@qq.com>
2020-04-13 22:46:47 +0800

    test

--- Database/Mysql/push.sh
+++ Database/Mysql/push.sh
@@ -19,7 +19,7 @@ book_bak="2020-04-13-my-bak-$now_book"
 #博客
 hxc_path="../../../huangxinchun.github.io/_posts"
 #博客图片地址
-images_bolg_path="$hxc_path/../assets/$images_path/"
+images_bolg_path="$hxc_path/../$images_path/"
 
 #更新自己的分支
 echo "更新开始！"
