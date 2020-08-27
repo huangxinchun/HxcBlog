21e7a9bcde0bb78b43b0abad455eb3bf43283068
hxc <173315279@qq.com>
2020-08-27 13:48:56 +0800

    push

--- Algorithm/lgt/link.php
+++ Algorithm/lgt/link.php
@@ -71,19 +71,56 @@ class link
     //在某个item后插入结点
     public function insertItem($data, $item)
     {
+        $node = new node($data);
 
+        $index_data = $this->header;
+        //
+        for ($n = 0; $n < $this->length(); $n++) {
+            if(empty($index_data->next)) {
+                continue;
+            }
+            $index_data = $index_data->next;
+            if($index_data->data == $item) {
+                break;
+            }
+        }
+        $node->next = $index_data->next;
+        $index_data->next = $node;
+        return $data;
     }
 
     //删除结点
     public function detele($index)
     {
-
+        $index_data = $this->header;
+        if ($index < 0 || $index > $this->length()) {
+            return false;
+        }
+        for ($n = 0; $n < $index; $n++) {
+            if($index_data->next == null) {
+                break;
+            }
+            $index_data = $index_data->next;
+        }
+        $next = $index_data->next;
+        //干掉
+        $index_data->next = $next->next;
+        return $index;
     }
 
     //获取某个结点
     public function getNode($index)
     {
+        $index_data = $this->header;
 
+        if ($index < 0 || $index > $this->length()) {
+            return false;
+        }
+        for ($n = 0; $n < $index; $n++) {
+            $index_data = $index_data->next;
+        }
+
+        return $index_data->data;
     }
 
     //获取长度
@@ -108,9 +145,8 @@ class link
     public function forLink()
     {
         $link_data = $this->header;
-
         for ($n = 0; $n < $this->length(); $n++) {
-            echo $link_data->data."--$n--". "/r/n";
+            echo "--$n--".$link_data->data."\r\n";
             $link_data = $link_data->next;
         }
     }
@@ -123,6 +159,8 @@ $link = new link();
 var_dump($link->insertIndex("11", 0));
 var_dump($link->insertIndex("22", 1));
 var_dump($link->insertIndex("33", 2));
-var_dump($link->insertIndex("44", 1));
+var_dump($link->insertItem(44, 22));
+//var_dump($link->detele(2));
+var_dump($link->getNode(2));
 $link->forLink();
 
